01624bea082eb9fb56a8991e3baf8fe83f5211fa
yuanbao97 <316261393@qq.com>
2021-01-27 11:55:39 +0800

    8.2.2 通知进阶功能

--- app/src/main/java/com/example/notificationtest/activity/MainActivity.java
+++ app/src/main/java/com/example/notificationtest/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.notificationtest.activity;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -7,6 +8,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
@@ -40,17 +42,34 @@ public class MainActivity extends BaseActivity {
 
     @OnClick(R.id.send_notice_button)
     public void onClick() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "State";
+            String channelName = "状态";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            createNotificationChannel(channelId, channelName, importance);
+        }
+
         Intent intent = new Intent(this, NotificationActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent, 0);
-        notification = new NotificationCompat.Builder(this, "AppTestNotificationId")
+        notification = new NotificationCompat.Builder(this, "default")
                 .setContentTitle("This is content title")
+                .setChannelId("State")
                 .setContentText("This is content text")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
                 .build();
 
+
+
         manager.notify(NOTIFICATION_TO_ACTIVITY, notification);
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName, int importance) {
+        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+        manager.createNotificationChannel(channel);
     }
 }
